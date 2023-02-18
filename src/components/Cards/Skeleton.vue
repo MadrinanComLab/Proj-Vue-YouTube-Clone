6758036adace6de3e7ba7ea19cc4d44b219d195a
MadrinanComLab <johncliffordmadrinan838@gmail.com>
2023-02-19 00:05:54 +0800

    Animation was added to skeleton loading

--- src/components/Cards/Skeleton.vue
+++ src/components/Cards/Skeleton.vue
@@ -1,5 +1,5 @@
 <template>
-    <div v-for="i in counter" :key="i">
+    <div v-for="i in counter" :key="i" class="animate-pulse">
         <div class="bg-[#252525] w-full h-[180px] rounded-lg mb-3"></div>
         <p class="bg-[#252525] w-11/12 h-[30px] rounded mb-3"></p>
         <p class="bg-[#252525] w-3/4 h-[30px] rounded"></p>
