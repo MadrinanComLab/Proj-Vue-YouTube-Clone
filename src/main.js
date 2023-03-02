2f2574b1a0f036c13ad1dd6a44adf2ea630f5d45
MadrinanComLab <johncliffordmadrinan838@gmail.com>
2023-03-02 21:55:47 +0800

    Vuex was added to the project

--- src/main.js
+++ src/main.js
@@ -1,17 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import VueMeta from "vue-meta";
+import store from "./store";
 import "./input.css";
 
-const vue_meta_options = {
-    /** OPTIONAL pluginOptions */
-    refreshOnceOnNavigation: true 
-};
-
-// REMOVED .use(VueMeta, vue_meta_options)
-
 createApp(App)
-    // .use(VueMeta, vue_meta_options)
     .use(router)
+    .use(store)
     .mount('#app');
