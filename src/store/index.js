9fa4b8f4d4242612a8454bbaf68dabdc480bb320
MadrinanComLab <johncliffordmadrinan838@gmail.com>
2023-03-09 23:16:47 +0800

    Data was moved to store

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,23 @@ import { createStore } from "vuex";
 
 export default createStore({
     state:{
+        error_message: "",
+        videos: [],
+        query: "",
+        next_page_token: "",
+        selected_tag: "All",
+        tags: [
+            "All",
+            "Music",
+            "Philippine Music",
+            "Playlist",
+            "Ed Sheeran",
+            "Live",
+            "Gaming",
+            "Contemporary R&B",
+            "News",
+            "Country Music"
+        ]
     },
     mutations:{
     },
