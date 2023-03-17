82b12a5f1cb874ce871c4c1049acaa6d53e324b8
MadrinanComLab <johncliffordmadrinan838@gmail.com>
2023-03-17 23:16:19 +0800

    Loading for search result was created

---
+++ src/views/Search.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="search_container">
+        <Header :display_tags="false"/>
+
+        <!-- px-[10%] -->
+        <div class="px-[30%] grid grid-cols-1 gap-2">
+            <Skeleton :counter="12"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+import Skeleton from "@/components/Cards/Skeleton.vue";
+
+export default {
+    name: "Search",
+    components: { Header, Skeleton },
+}
+</script>
