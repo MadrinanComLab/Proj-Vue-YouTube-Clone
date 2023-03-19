eee92e86ab6361fdddf39d80feca3dd1579b3d28
MadrinanComLab <johncliffordmadrinan838@gmail.com>
2023-03-19 22:01:18 +0800

    Search bar can now redirect user to search result page

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -2,6 +2,8 @@
     <div id="search_container">
         <Header :display_tags="false"/>
 
+        <h2 class="text-white">Query: {{ $route.query.search_query }}</h2>
+
         <!-- px-[10%] -->
         <div class="px-[30%] grid grid-cols-1 gap-2">
             <Skeleton :counter="12"/>
