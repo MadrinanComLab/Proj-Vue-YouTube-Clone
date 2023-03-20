659db9ce8bc349487ce078fe6010e627fb03cf63
MadrinanComLab <johncliffordmadrinan838@gmail.com>
2023-03-20 22:55:31 +0800

    Rendering HTML entities for YouTube channels was added

--- src/components/Cards/YouTubeChannel.vue
+++ src/components/Cards/YouTubeChannel.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="w-full text-center">
+        <!--/ Reference for rendering HTML entities in Vue: https://stackoverflow.com/questions/46756802/vue-js-auto-convert-html-and-unicode-entities /-->
         <img :src="thumbnail" alt="" class="rounded-full max-h-[200px] m-auto">
-        <h2 class="text-white font-bold m-3">{{ title }}</h2>
+        <h2 class="text-white font-bold m-3" v-html="title"></h2>
     </div>
 </template>
 
