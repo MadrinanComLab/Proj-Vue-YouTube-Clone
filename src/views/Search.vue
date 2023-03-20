9fd76d3b023b85def133776ef392ee15ac5c5104
MadrinanComLab <johncliffordmadrinan838@gmail.com>
2023-03-21 06:57:20 +0800

    Pagination in search result was created

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -12,6 +12,8 @@
                 <YouTubeChannel v-if="video.id.kind === 'youtube#channel'" :thumbnail="video.snippet.thumbnails.high.url" :title="video.snippet.title"/>
                 <YouTubeVideo v-else :thumbnail="video.snippet.thumbnails.high.url" :title="video.snippet.title" :channel_title="video.snippet.channelTitle"/>
             </div>
+
+            <Skeleton :counter="1"/>
         </div>
     </div>
 </template>
@@ -26,6 +28,8 @@ export default {
     name: "Search",
     components: { Header, Skeleton, YouTubeVideo, YouTubeChannel },
     mounted(){
+        /** This will be used for detecting if the bottom of the page was reached */
+        window.addEventListener("scroll", (e) => this.$store.dispatch("detectBottomPage", { event: e }));
     }
 }
 </script>
