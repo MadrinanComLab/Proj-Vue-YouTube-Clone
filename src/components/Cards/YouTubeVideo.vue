cf8b22bdd27ecb6bd0c87bab35b7821b7709616b
MadrinanComLab <johncliffordmadrinan838@gmail.com>
2023-03-22 22:33:07 +0800

    Event that will handle the dynamic document title for selected video was created

--- src/components/Cards/YouTubeVideo.vue
+++ src/components/Cards/YouTubeVideo.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="w-full">
         <!--/ Reference for rendering HTML entities in Vue: https://stackoverflow.com/questions/46756802/vue-js-auto-convert-html-and-unicode-entities /-->
-        <router-link :to="`/watch?v=${ video_id }`">
+        <router-link :to="`/watch?v=${ video_id }`" @click="handleSelectedVideo(title)">
             <img class="w-full min-h-[245px] mb-2" :src="thumbnail" alt="">
             <h2 class="text-white font-bold" v-html="title"></h2>
             <p class="text-[#888] text-sm" v-html="channel_title"></p>
@@ -12,6 +12,12 @@
 <script>
 export default {
     name: "YouTubeVideo",
-    props: [ "thumbnail", "title", "channel_title", "video_id" ]
+    props: [ "thumbnail", "title", "channel_title", "video_id" ],
+    methods: {
+        handleSelectedVideo(video_title){
+            /** Save the title of selected video in the store */
+            this.$store.commit("setVideoTitle", video_title);
+        }
+    }
 }
 </script>
