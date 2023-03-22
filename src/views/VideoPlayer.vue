3a850857ba9b9e2b42ec0568a415cdca09add376
MadrinanComLab <johncliffordmadrinan838@gmail.com>
2023-03-23 06:51:58 +0800

    Other videos was displayed in VideoPlayer

--- src/views/VideoPlayer.vue
+++ src/views/VideoPlayer.vue
@@ -2,23 +2,38 @@
     <div id="video_player_container">
         <Header :display_tags="false"/>
 
-        <div class="px-[10%]">
-            <!--/
-            Reference for accessing the video:
-            * https://stackoverflow.com/questions/26046567/youtube-api-get-a-video
-            * https://developers.google.com/youtube/iframe_api_reference
-            /-->
-            <iframe className="w-100" :src="`http://www.youtube.com/embed/${ selected_video_id }?enablejsapi=1&origin=http://example.com`"></iframe>
+        <div class="px-[10%] grid grid-container grid-cols-7 gap-4">
+            <div class="col-span-5">
+                <!--/
+                Reference for accessing the video:
+                * https://stackoverflow.com/questions/26046567/youtube-api-get-a-video
+                * https://developers.google.com/youtube/iframe_api_reference
+                /-->
+                <iframe className="w-full h-[500px]" :src="`http://www.youtube.com/embed/${ selected_video_id }?enablejsapi=1&origin=http://example.com`"></iframe>
+
+                <!-- Comment section -->
+            </div>
+
+            <div class="col-span-2 grid grid-cols-1 gap-2">
+                <div v-for="(video, i) in $store.state.videos" :key="i">
+                    <!-- Used different component for this. The other suggested videos are displayed differently -->
+                    <YouTubeChannel v-if="video.id.kind === 'youtube#channel'" :thumbnail="video.snippet.thumbnails.high.url" :title="video.snippet.title"/>
+                    <YouTubeVideo v-else :thumbnail="video.snippet.thumbnails.high.url" :title="video.snippet.title" :channel_title="video.snippet.channelTitle" :video_id="video.id.videoId"/>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 import Header from "@/components/Header.vue";
+import Skeleton from "@/components/Cards/Skeleton.vue";
+import YouTubeVideo from "@/components/Cards/YouTubeVideo.vue";
+import YouTubeChannel from "@/components/Cards/YouTubeChannel.vue";
 
 export default {
     name: "VideoPlayer",
-    components: { Header },
+    components: { Header, Skeleton, YouTubeVideo, YouTubeChannel },
     data() {
         return {
             /** This variable was declared for better readability */
