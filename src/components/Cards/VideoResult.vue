d6df91ada1412b9a017ba6d890af5da4aa3dfa44
MadrinanComLab <johncliffordmadrinan838@gmail.com>
2023-03-30 22:29:13 +0800

    VideoResult converted into dynamic to be used in both search result and video player components

--- src/components/Cards/VideoResult.vue
+++ src/components/Cards/VideoResult.vue
@@ -3,11 +3,11 @@
     <!--/ Reference for rendering HTML entities in Vue: https://stackoverflow.com/questions/46756802/vue-js-auto-convert-html-and-unicode-entities /-->
     <router-link :to="`/watch?v=${ video_id }`" @click="handleSelectedVideo(title)">
         <div class="w-full grid grid-container grid-cols-7 gap-4">
-            <img class="w-full min-h-[170px] mb-2 col-span-3" :src="thumbnail" alt="">
+            <img :class="`w-full min-h-[${thumbnail_height}] mb-2 col-span-3`" :src="thumbnail" alt="">
             <div class="col-span-4">
-                <h2 class="text-white font-bold" v-html="title"></h2>
-                <p class="text-[#888] text-sm" v-html="channel_title"></p>
-                <p class="text-[#888] text-sm mt-3" v-html="description"></p>
+                <h3 class="text-white font-bold text-sm" v-html="title"></h3>
+                <p class="text-[#888] text-xs" v-html="channel_title"></p>
+                <p class="text-[#888] text-sm mt-3" v-if="show_description" v-html="description"></p>
             </div>
         </div>
     </router-link>
@@ -16,9 +16,10 @@
 <script>
 export default {
     name: "VideoResult",
-    props: [ "thumbnail", "title", "channel_title", "video_id", "description" ],
+    props: [ "thumbnail", "title", "channel_title", "video_id", "description", "show_description", "thumbnail_height" ],
     methods: {
         handleSelectedVideo(video_title){
+            //TODO: This same function was also used in Home component. Move this on the Vuex store later...
             /** Save the title of selected video in the store */
             this.$store.commit("setVideoTitle", video_title);
         }
