04eff791e4abd64cb8f6c547d6379b6ac05a69df
MadrinanComLab <johncliffordmadrinan838@gmail.com>
2023-04-02 23:52:19 +0800

    References used to create a watcher was included

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,11 @@
 export default {
   name: 'App',
   watch: {
+      /**
+       * References used for watcher:
+       * https://stackoverflow.com/questions/46402809/vuejs-event-on-route-change
+       * https://vuejs.org/guide/essentials/watchers.html#basic-example
+       */
       $route(to, from){
             /** Removed cached videos since each page has it own videos to load */
             localStorage.removeItem("yt_videos");
